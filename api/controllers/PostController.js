ad4d48481894261f89894209c78ef6dafa23a9ac
Vivek Bharatha <vivek.bharatha@gmail.com>
2015-09-21 01:06:44 +0530

    Added real time updates to other users using PubSub. Added new post validations.

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -7,21 +7,29 @@
 
 module.exports = {
 
-  createPost : function (req, res, next) {
+  createPost: function (req, res, next) {
     var title = req.param('title'), content = req.param('content');
 
     if (!title || !content) return res.json({error: 'Missing Fields'});
 
     Post.create({title: title, content: content}).exec(function (err, post) {
       if (err) return res.json({error: err});
+      if (req.isSocket) {
+        Post.publishCreate(post, req);
+        console.log('A new post -> ' + post.title + ' has been created!');
+      }
 
       return res.json({success: 'Post to world', post: post});
     });
   },
   getPosts: function (req, res, next) {
+
     Post.find().sort('createdAt DESC').exec(function (err, posts) {
       if (err) return res.json({error: err});
-
+      if (req.isSocket) {
+        Post.watch(req);
+        console.log('User with socket id ' + sails.sockets.id(req) + ' is now subscribed to the model class \'post\'.');
+      }
       return res.json({posts: posts});
     });
   }
--- assets/scripts/controllers/HomeController.js
+++ assets/scripts/controllers/HomeController.js
@@ -7,9 +7,16 @@ angular.module('instots')
     var toastOptions = {position: 'top right', hideDelay: '2000'};
 
     $scope.getPosts = function(){
-      $http.get('/posts').then(function(response){
+      /*$http.get('/posts').then(function(response){
         if(response.data.posts) {
           $scope.posts = response.data.posts;
+          $mdToast.show($mdToast.simple(toastOptions).content('Loaded latest posts from world!'));
+        }
+      });*/
+
+      io.socket.get('/posts', function(resObj){
+        if(resObj.posts) {
+          $scope.posts = resObj.posts;
           $mdToast.show($mdToast.simple(toastOptions).content('Loaded latest posts from world!'));
         }
       });
@@ -21,24 +28,54 @@ angular.module('instots')
       $scope.newPost = {};
       $scope.newPost.title = '';
       $scope.newPost.content = '';
+      $scope.newPost.contentError = false;
     };
 
-    $scope.post = function() {
+    $scope.post = function(newPostForm) {
+      if($scope.newPost.content=='') {
+        $scope.newPost.contentError = true;
+        return;
+      }
+
       var postObject = {
         title: $scope.newPost.title,
         content: $scope.newPost.content
       };
 
-      $http.post('/post', postObject).then(function(response){
+      /*$http.post('/post', postObject).then(function(response){
         if(response.data.success) {
           $mdToast.show($mdToast.simple(toastOptions).content('Successfully shared your post to world!'));
-          $scope.posts.unshift(postObject);
+          $scope.posts.unshift(response.data.post);
           $scope.reset();
         } else {
           $mdToast.show($mdToast.simple(toastOptions).content('An error occurred, try again! '));
         }
+      });*/
+
+      io.socket.post('/post', postObject, function(resObj){
+        if(resObj.success) {
+          $mdToast.show($mdToast.simple(toastOptions).content('Successfully shared your post to world!'));
+          $scope.posts.unshift(resObj.post);
+          $scope.reset();
+        } else {
+          $mdToast.show($mdToast.simple(toastOptions).content('An error occurred, try again! '));
+        }
+        newPostForm.$setPristine();
+        newPostForm.$setUntouched();
       });
     };
 
     $scope.reset();
+
+    /**
+     * Sails socket events
+     */
+
+    io.socket.on('post', function(obj){
+      if(obj.verb == 'created') {
+        $mdToast.show($mdToast.simple(toastOptions).content('A new post'));
+        $scope.posts.unshift(obj.data);
+      }
+    });
+
   });
